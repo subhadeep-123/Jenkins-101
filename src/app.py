0ff10350cc143a6b14b60d636dc13e536b81d33c
Subhadeep Banerjee <subhadeep762@gmail.com>
2023-08-02 02:16:30 +0530

    Add flask api

---
+++ src/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def api():
+    return jsonify({
+        "name": "Jenkins-101",
+        "description": "Dockerized Python flask application, CI/CD with Jenkins and deployed to AWS ECR",
+        "version": '1.0.0'
+    })
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=False)
